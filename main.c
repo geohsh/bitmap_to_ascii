e2478469d7fa1572511eac2160f4f34c6102c065
George He <george-he1998@hotmail.com>
2017-02-05 12:44:35 -0500

    The source file

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void bit_map_convert(char* in_pic, char* out_txt)
+{
+    FILE * pic = fopen(in_pic, "rb");
+    const char gs[18] = "$@8&MV+<|\"=~;`. ";
+
+    if (pic == NULL){
+        fprintf(stderr, "cannot open the pic");
+    }
+    unsigned char bitmap_header [54];
+    fread(bitmap_header, sizeof(unsigned char), 54, pic);
+
+    int width = (*(int*)&bitmap_header[18]);
+    int height = *(int*)&bitmap_header[22];
+    int pic_size = *(int*)&bitmap_header[34];
+    int true_width = 3 * width;
+
+    unsigned char* orig_pic = (unsigned char*) malloc(pic_size);
+    fread(orig_pic, sizeof(unsigned char), pic_size, pic);
+
+    int to_grey[height / 4][width / 2];
+
+
+    for (int i = 0; i < height; i += 4){
+        for (int j = 0; j < width; j += 2){
+            to_grey[i / 4][j / 2] =
+            (orig_pic[i * true_width + j * 3]
+             + orig_pic[i * true_width + j * 3 + 1]
+             + orig_pic[i * true_width + j * 3 + 2]) / 3;
+        }
+    }
+
+    FILE * output_txt = fopen(out_txt, "w");
+    for(int i = height / 4 - 1; i >= 0 ; i--){
+        for (int j = 0 ; j < width / 2 ; j++){
+            fputc ( gs[(to_grey[i][j] / 17)], output_txt);
+        }
+        fputc('\n', output_txt);
+    }
+
+    free(orig_pic);
+    fclose(output_txt);
+    fclose(pic);
+}
+
+int main(void){
+    char input_pic[20] = {'\0'};
+    char out_put[20] = {'\0'};
+    printf("Please in put the file name you want to convert in the following format\n");
+    printf("file_name_of_picture file_name_of_output");
+    scanf("%s %s", &input_pic, &out_put);
+    printf("working...\n");
+
+    bit_map_convert(input_pic, out_put);
+    printf("done\n");
+
+    printf("Press any key to continue...\n");
+    getchar();
+
+    return 0;
+}
